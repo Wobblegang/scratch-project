2d1e3426b887565ebf7d127369a48cf48af4f4db
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-06-18 14:10:47 -0700

    complete add

--- client/src/components/Add/Add.js
+++ client/src/components/Add/Add.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Add.scss';
+
 
 //path to add <Route path='/library/:charterNumber/addBook' element={<Add />} />
 const Add = () => {
@@ -18,47 +20,50 @@ const Add = () => {
     setNewGenre(e.target.value);
   };
 
-  const saveNowBook = (e) => {
-    e.preventDefault();
+  const navigate = useNavigate();
+
+  const saveBook = (e) => {
     const newBook = {
       title: newTitle,
       author: newAuthor,
       genre: newGenre,
     };
-    // addNewBook(newBook);
-    // post reqest to store new book
-
     setNewTitle('');
     setNewAuthor('');
     setNewGenre('');
-    setNewFirstPublishedYear('');
+
+    fetch(`/api/library/${charterNumber}/addBook`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newBook),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error: unable to add book to library');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if(data.success) {
+        navigate(`/library/${charterNumber}`);
+      }
+    })
   };
 
   return (
-    <div className='add-box'>
-      <form>
-        <label>Book Title:</label>
-        <br />
-        <input type='text' onChange={getNewTitle} value={newTitle} />
-        <br />
-        <label>Author:</label>
-        <br />
-        <input type='text' onChange={getNewAuthor} value={newAuthor} />
-        <br />
-        <label>Genre:</label>
-        <br />
-        <input type='text' onChange={getNewGenre} value={newGenre} />
-        <br />
-        <label>First Published Year:</label>
-        <br />
-        <input
-          type='text'
-          onChange={getNewFirstPublishedYear}
-          value={newFirstPublishedYear}
-        />
-        <br />
-        <button onClick={saveNowBook}>Submit</button>
-      </form>
+    <div className='addBody'>
+      <h1>Add a Book</h1>
+      <h2>Little Free Library Charter Number: {charterNumber}</h2>
+      <label>Book Title:</label>
+      <input type='text' onChange={getNewTitle} value={newTitle} placeholder='Book Title'/>
+      <label>Author:</label>
+      <input type='text' onChange={getNewAuthor} value={newAuthor} placeholder='Author'/>
+      <label>Genre:</label>
+      <input type='text' onChange={getNewGenre} value={newGenre} placeholder='Genera'/>
+      <button onClick={saveBook}>Add Book</button>
+      <Link to={`/library/${charterNumber}`}>Back to Library</Link>
     </div>
   );
 };
