a22d245ee6ed1e18fe6c7e8194fd77a832ca0147
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-06-19 21:52:04 -0700

    addbook adds to db but not functioning as intented

--- client/src/components/AddBook/AddBookModal.js
+++ client/src/components/AddBook/AddBookModal.js
@@ -6,17 +6,17 @@ import './AddBookModal.scss';
 const AddBookModal = (props) => {
   const { charterNumber } = useParams();
 
-  // Do not show AddBookModal if props.show is false
-  if(!props.show) {
-    return null;
-  };
-
   // State
   const [bookData, setBookData] = useState({
     title: '',
     author: '',
     genre: '',
   });
+
+  // Do not show AddBookModal if props.show is false
+  if(!props.show) {
+    return null;
+  };
 
   // Update bookData state on input change
   const handleChange = (e) => {
@@ -32,18 +32,25 @@ const AddBookModal = (props) => {
 
     const newBook = bookData;
 
-    setNewTitle('');
-    setNewAuthor('');
-    setNewGenre('');
+    setBookData({
+      title: '',
+      author: '',
+      genre: '',
+    });
 
-    fetch(`/api/library/${charterNumber}/addBook`, {
-      method: 'POST',
+    // setNewTitle('');
+    // setNewAuthor('');
+    // setNewGenre('');
+
+    fetch(`/api/library/${charterNumber}`, {
+      method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(newBook),
     })
     .then(response => {
+      console.log('response from add book fetch:', response);
       if (!response.ok) {
         throw new Error('Error: unable to add book to library');
       }
@@ -54,6 +61,9 @@ const AddBookModal = (props) => {
         props.onClose();
       }
     })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   };
 
   return (
@@ -62,18 +72,36 @@ const AddBookModal = (props) => {
         <button className='btn close-btn' onClick={props.onClose}>&times;</button>
         <h2 className='modal-h2'>Add a Book</h2>
         <p>Little Free Library Charter Number: {charterNumber}</p>
-        <form className='addBook-form' onSubmit={saveBook}>
+        <div className='addBook-form'>
           <label>Book Title:</label>
           <input type='text' name='title' value={bookData.title} placeholder='Book Title' onChange={handleChange} />
           <label>Author:</label>
           <input type='text' name='author' value={bookData.author} placeholder='Author' onChange={handleChange} />
           <label>Genre:</label>
           <input type='text' name='genre' value={bookData.genre} placeholder='Genera' onChange={handleChange} />
-          <button className='addBook-btn' type='submit'>Add Book</button>
-        </form>
+          <button className='addBook-btn' onClick={saveBook}>Add Book</button>
+        </div>
       </div>
     </div>
   );
+  // return (
+  //   <div className='modal-overlay' onClick={props.onClose}>
+  //     <div className='modal' onClick={e => e.stopPropagation()}>
+  //       <button className='btn close-btn' onClick={props.onClose}>&times;</button>
+  //       <h2 className='modal-h2'>Add a Book</h2>
+  //       <p>Little Free Library Charter Number: {charterNumber}</p>
+  //       <form className='addBook-form' onSubmit={saveBook}>
+  //         <label>Book Title:</label>
+  //         <input type='text' name='title' value={bookData.title} placeholder='Book Title' onChange={handleChange} />
+  //         <label>Author:</label>
+  //         <input type='text' name='author' value={bookData.author} placeholder='Author' onChange={handleChange} />
+  //         <label>Genre:</label>
+  //         <input type='text' name='genre' value={bookData.genre} placeholder='Genera' onChange={handleChange} />
+  //         <button className='addBook-btn' type='submit'>Add Book</button>
+  //       </form>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default AddBookModal;
